d4389e85f4ddb49c0a7ecf26de76bd8ddab2a6bd
Alan Neads <alan@neadsdev.com>
2017-10-09 18:14:19 -0700

    Added producer and consumer functionality to concurrency.

--- assignment1/concurrency.c
+++ assignment1/concurrency.c
@@ -1,9 +1,23 @@
+/*
+Authors:        Alan Neads & David Corbelli
+Course:         cs444 f17       
+References: Assembly code used to determine if CPU supports rdrand is referenced in-part from 
+                        https://codereview.stackexchange.com/questions/147656/checking-if-cpu-supports-rdrand 
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <limits.h>
 
-#include "mt19937.c"
+#include "mt19937ar.c"
+
+#define bit_RDRND   (1 << 30)
 
 void setupSignalCatching();
 void signalCatch( int signal );
@@ -15,6 +29,9 @@ int getRandomNumberRange( int floor, int ceil );
 
 void* producerThread( void *params );
 void* consumerThread( void *params );
+
+int checkRand();
+#define _rdrand_generate(x) ({ unsigned char err; asm volatile("rdrand %0; setc %1":"=r"(*x), "=qm"(err)); err;})
 
 struct bufferItem {
     int number;
@@ -36,13 +53,14 @@ pthread_cond_t consumerCondition;
 int main( int argc, char** argv ) {
     producerJobListIndex = 0;
     consumerJobListIndex = 0;
-
+    init_genrand(time(NULL));
+    
     pthread_mutex_init( &bufferJobListLock, NULL );
 
     setupSignalCatching();
     setupThreads();
-
     waitThreads();
+   
     return 0;
 };
 
@@ -73,14 +91,62 @@ void setupThreads() {
 };
 
 void* producerThread( void *params ) {
+    int producerSleepDuration;
+    struct bufferItem newItem;
+    pid_t pid = syscall(SYS_gettid);
+    
     while(1) {
+        pthread_mutex_lock(&bufferJobListLock);
+        
+        newItem.number = getRandomNumberRange( 0, INT_MAX - 1 );
+        newItem.sleepDuration = getRandomNumberRange( 2, 9 );
+        producerSleepDuration = getRandomNumberRange( 3, 7 );
 
+        while( producerJobListIndex >= 31 ) {
+            pthread_cond_wait( &producerCondition, &bufferJobListLock );
+        }
+
+        bufferJobList[producerJobListIndex] = newItem;
+        producerJobListIndex += 1;
+
+        if ( producerJobListIndex > 31 ) {
+            producerJobListIndex = 0;
+        }
+
+        pthread_cond_signal(&consumerCondition);
+        pthread_mutex_unlock(&bufferJobListLock);
+
+        printf("[PRODUCER][PID %lu] Produced number %d to the queue, will sleep for %d seconds.\n", pid, newItem.number, producerSleepDuration );
+        
+        sleep(producerSleepDuration);
     }
 };
 
 void* consumerThread( void *params ) {
-    while(1) {
+    int number;
+    int sleepDuration;
+    pid_t pid = syscall(SYS_gettid);
 
+    while(1) {
+        pthread_mutex_lock(&bufferJobListLock);
+
+        while( producerJobListIndex == 0 || producerJobListIndex == consumerJobListIndex ) {
+            pthread_cond_wait( &consumerCondition, &bufferJobListLock );
+        }
+
+        number = bufferJobList[consumerJobListIndex].number;
+        sleepDuration = bufferJobList[consumerJobListIndex].sleepDuration;
+
+        consumerJobListIndex += 1;
+        if ( consumerJobListIndex > 31 ) {
+            consumerJobListIndex = 0;
+        }
+        pthread_cond_signal(&producerCondition);
+        pthread_mutex_unlock(&bufferJobListLock);
+
+        printf("[CONSUMER][PID %lu] Consumed number %d, will sleep for %d seconds.\n", pid, number, sleepDuration );
+
+        sleep(sleepDuration);
     }
 };
 
@@ -91,5 +157,31 @@ void waitThreads() {
 };
 
 int getRandomNumberRange( int floor, int ceil ) {
-    
+    int num = 0;
+    if(checkRand() == 0){
+        _rdrand_generate(&num);
+    }
+    else{
+        num = abs(genrand_int32());
+    }
+    num = floor + num % (ceil + 1 - floor);
+    return num;
+};
+
+int checkRand() {
+    int eax;
+    int ecx;
+    eax = 0x01;
+    __asm__ __volatile__("cpuid;": "=a"(eax), "=c"(ecx) : "a"(eax));
+                                                 
+         
+    if(ecx & bit_RDRND){
+        //use rdrand
+        return 0;
+    }
+    else{
+        //use mt19937
+        return 1;
+    }
+        
 };
